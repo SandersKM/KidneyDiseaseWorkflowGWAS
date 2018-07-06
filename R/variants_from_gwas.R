175a417eab6f43cb000e5bb8bb5f7512e5210c91
Kate Sanders <SandersKM@Hendrix.edu>
2018-07-06 13:03:22 -0400

    Start and end positions for gene versions GRCh37 and 38. Variant relative location to gene.

--- R/variants_from_gwas.R
+++ R/variants_from_gwas.R
@@ -6,9 +6,22 @@
 library(httr)
 library(xml2)
 library(jsonlite)
+library(biomaRt)
 
 # Enter the HGNC symbol of the gene of interest below
 gene.of.interest <- "MUC1"
+
+# Getting Start and End information for gene of interest
+ensembl.GRCh37 = useEnsembl(biomart="ensembl", dataset="hsapiens_gene_ensembl", GRCh=37)
+ensembl.GRCh38 = useEnsembl(biomart="ensembl", dataset="hsapiens_gene_ensembl")
+gene.of.interest.start.GRCh37 <- getBM("start_position", filters="hgnc_symbol",
+                                       values=gene.of.interest, mart=ensembl.GRCh37)[[1]]
+gene.of.interest.end.GRCh37 <- getBM("end_position", filters="hgnc_symbol",
+                                     values=gene.of.interest, mart=ensembl.GRCh37)[[1]]
+gene.of.interest.start.GRCh38 <- getBM("start_position", filters="hgnc_symbol",
+                                       values=gene.of.interest, mart=ensembl.GRCh38)[[1]]
+gene.of.interest.end.GRCh38 <- getBM("end_position", filters="hgnc_symbol",
+                                     values=gene.of.interest, mart=ensembl.GRCh38)[[1]]
 
 # Access the GWAS cataloge records through their API
 gwas.api.url <- paste("https://www.ebi.ac.uk/gwas/labs/rest/api/singleNucleotidePolymorphisms/search/findByGene?geneName=",
@@ -29,7 +42,6 @@ gwas_variants$minor.allele <- numeric(dim(gwas_variants)[1])
 # variant (such as a SNP). The global MAF is calculated using the allele frequences
 # across all 1000 Genomes Phase I populations. - Ensembl.org
 gwas_variants$MAF <- numeric(dim(gwas_variants)[1])
-gwas_variants$most.severe.consequence <- character(dim(gwas_variants)[1])
 gwas_variants$synonyms <- character(dim(gwas_variants)[1])
 # Ensembl API imports Variants (including SNPs and indels)from dbSNP
 variant_from_ensembl <- function(n){
@@ -40,10 +52,12 @@ variant_from_ensembl <- function(n){
   gwas_variants$ancestral.allele[n] <<- cont$ancestral_allele
   gwas_variants$minor.allele[n] <<- cont$minor_allele
   gwas_variants$MAF[n] <<- cont$MAF
-  gwas_variants$most.severe.consequence[n] <<- cont$most_severe_consequence
   gwas_variants$synonyms[n] <<- gsub(",", ";", toString(cont$synonyms))
   }
 sapply(1:dim(gwas_variants)[1],variant_from_ensembl)
-
-
-
+gwas_variants$Intergenic <- sapply(1:dim(gwas_variants)[1], function(n){
+  gwas_variants$genomicContexts[[n]]$isIntergenic[which(i$gene$geneName == "MUC1")[1]][[1]]})
+gwas_variants$Upstream <- sapply(1:dim(gwas_variants)[1], function(n){
+  gwas_variants$genomicContexts[[n]]$isUpstream[which(i$gene$geneName == "MUC1")[1]][[1]]})
+gwas_variants$Downstream <- sapply(1:dim(gwas_variants)[1], function(n){
+  gwas_variants$genomicContexts[[n]]$isDownstream[which(i$gene$geneName == "MUC1")[1]][[1]]})
