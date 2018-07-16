9739fbdcd8d420c04ecf81196b33fe48ea3f5abb
Kate Sanders <SandersKM@Hendrix.edu>
2018-07-16 14:30:09 -0400

    Converting mouse genes to human genes

--- R/SingleCell_eQTL_GWAS_combined.R
+++ R/SingleCell_eQTL_GWAS_combined.R
@@ -3,10 +3,16 @@
 # BSRP 2018
 ###################
 
-
+library(pheatmap)
 library(gwascat)
+library(scater)
 library(readxl)
+library(stringi)
 
+# Replace the following with the specific gwas traits of interest
+gwas.traits.of.interest <- c("Chronic kidney disease (chronic kidney disease vs normal
+                             or mildly reduced eGFR) in type 1 diabetes","Chronic kidney disease
+                             (severe chronic kidney disease vs normal kidney function) in type 1 diabetes")
 
 # Run this only if this value doesn't already exist. It takes a while.
 # current_gwascat <- makeCurrentGwascat(genome = "GRCh37")
@@ -19,12 +25,24 @@ single.cell.all.genes <- read_excel("/Users/ksanders/Downloads/aar2131_Table_S3.
                                     sheet= "summary_for_all_genes")[-c(1), ]
 
 # Renaming columns
-colnames(single.cell.all.genes)[colnames(single.cell.all.genes)=="X__1"] <- "gene"
+colnames(single.cell.all.genes)[colnames(single.cell.all.genes)=="X__1"] <- "mouse.gene"
 
 sapply(2:dim(single.cell.all.genes)[2], function(n){
   name.paren <- paste(strsplit(names(single.cell.all.genes)[n], split = " ")[[1]][-1], collapse = "-")
   colnames(single.cell.all.genes)[colnames(single.cell.all.genes)==names(single.cell.all.genes)[n]] <<-
-    substring(name.paren, first = 2, last = str_length(name.paren) - 1)
+    substring(name.paren, first = 2, last = stri_length(name.paren) - 1)
 })
 
-s<- c("Chronic kidney disease","Chronic kidney disease (chronic kidney disease vs normal or mildly reduced eGFR) in type 1 diabetes","Chronic kidney disease (severe chronic kidney disease vs normal kidney function) in type 1 diabetes","Chronic kidney disease and serum creatinine levels","Diabetic kidney disease","Fractional excretion of metabolites in chronic kidney disease","Glomerular filtration rate in chronic kidney disease","Immunoglobulin light chain (AL) amyloidosis (heart and kidney involvement)","Immunoglobulin light chain (AL) amyloidosis (kidney involvement)","Kidney cancer","Kidney disease (early and late stages) in type 1 diabetes","Kidney disease (early stage) in type 1 diabetes","Kidney disease (end stage renal disease vs non-end stage renal disease) in type 1 diabetes","Kidney disease (end stage renal disease vs normoalbuminuria) in type 1 diabetes","Kidney disease (late stage) in type 1 diabetes","Kidney function decline traits","Kidney stones","Proteinuria and chronic kidney disease","Proteinuria in chronic kidney disease","Renal function and chronic kidney disease","Serum metabolite concentrations in chronic kidney disease","Serum metabolite ratios in chronic kidney disease","Tacrolimus trough concentration in kidney transplant patients","Urinary metabolite concentrations in chronic kidney disease","Urinary metabolite ratios in chronic kidney disease")
+# Converting Mouse genes to Human Genes
+mart <- get0("mart", ifnotfound=useEnsembl(biomart="ensembl", dataset="hsapiens_gene_ensembl", GRCh=37))
+mouse.mart <- get0("mouse.mart", ifnotfound=useEnsembl(biomart="ensembl", dataset="mmusculus_gene_ensembl", GRCh=37))
+single.cell.all.genes <- cbind(human.gene = "", single.cell.all.genes)
+mouse.to.human <- function(x){
+  genesV2 <- getLDS(attributes = "mgi_symbol", filters = "mgi_symbol", values = x , mart = mouse.mart,
+                    attributesL = "hgnc_symbol", martL = mart, uniqueRows=T)
+  return(unique(genesV2[, 2]))
+}
+single.cell.all.genes$human.gene <- sapply(single.cell.all.genes$mouse.gene, mouse.to.human)
+# search for gwas hits
+gwas.hits.by.trait <- subsetByTraits(current_gwascat, tr = gwas.traits.of.interest)
+
